f2c7ba8cb080f715b138151c73b90a9400aadad9
Liso Gallo <liso@riseup.net>
2015-09-04 12:46:53 -0300

    [FIX] Minor changes

--- odoo_csv2xml.py
+++ odoo_csv2xml.py
@@ -2,14 +2,14 @@
 
 import re
 import sys
-import os
+from os import chdir, path
 import glob
 
 # delimiter used in the CSV file(s)
-delimiter = ","
+DELIMITER = ","
 
 # 4 spaces tabulation
-tab = 4 * ' '
+TAB = 4 * ' '
 
 def reference_match(field, regex=re.compile(r'([a-z]*_id\/id)$')):
     return regex.match(field)
@@ -22,13 +22,13 @@ if len(sys.argv) == 2:
         csvFiles = [arg]
     # If a folder path then convert all CSV files in the that folder
     else:
-        os.chdir(arg)
+        chdir(arg)
         csvFiles = glob.glob('*.csv')
 # If no command-line argument then convert all CSV files in the current folder
 elif len(sys.argv) == 1:
     csvFiles = glob.glob('*.csv')
 else:
-    os._exit(1)
+    sys.exit()
 
 for csvFileName in csvFiles:
     xmlFile = csvFileName[:-4] + '.xml'
@@ -37,13 +37,14 @@ for csvFileName in csvFiles:
     csvFile = open(csvFileName, 'rb')
     csvData = csvFile.readlines()
     csvFile.close()
-    tags = csvData.pop(0).strip().replace(' ', '_').split(delimiter)
+    tags = csvData.pop(0).strip().replace(' ', '_').split(DELIMITER)
     xmlData = open(xmlFile, 'w')
     xmlData.write('<?xml version="1.0" encoding="utf-8"?>' + '\n')
 
     # There must be only one top-level tag
     xmlData.write('<openerp>' + '\n')
-    xmlData.write(tab + '<data noupdate="0">' + '\n')
+    xmlData.write(TAB + '<data noupdate="0">' + '\n')
+
 
     for row in csvData:
         rowData = row.strip().split(delimiter)
@@ -54,16 +55,16 @@ for csvFileName in csvFiles:
 
             if tags[i]:
                 if reference_match(tags[i]) and rowData[i]:
-                    xmlData.write(tab*3 + '<field name="' +
+                    xmlData.write(TAB*3 + '<field name="' +
                                   tags[i].split('/', 1)[0] +
                                   '"' + ' ref="' + rowData[i] + '"/>' + '\n')
                 else:
-                    xmlData.write(tab*3 + '<field name="' +
+                    xmlData.write(TAB*3 + '<field name="' +
                                   tags[i].split('/', 1)[0] +
                                   '">' + rowData[i] + '</field>' + '\n')
 
-        xmlData.write(tab*2 + '</record>' + '\n')
+        xmlData.write(TAB*2 + '</record>' + '\n')
 
-    xmlData.write('\n' + tab + '</data>' + '\n')
+    xmlData.write('\n' + TAB + '</data>' + '\n')
     xmlData.write('</openerp>' + '\n')
     xmlData.close()
